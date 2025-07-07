1f53829aab41d658977736ee566b27626715a8f9
Smid1 <nicollassmid1@gmail.com>
2025-07-07 00:50:10 -0300

    testes corringindo erros

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
